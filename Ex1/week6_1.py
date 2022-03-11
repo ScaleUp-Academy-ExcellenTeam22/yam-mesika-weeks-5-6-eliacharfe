ba121a5259f0ec2c776c4ce3f01451cd3bfb04ab
Eliachar Feig <efeig15@gmail.com>
2022-03-12 01:47:49 +0200

    שטחולנדיה

--- Ex1/week6_1.py
+++ Ex1/week6_1.py
@@ -1,7 +1,6 @@
 import time
 
-
-# Fast and Reliable
+# -- Fast and Reliable
 
 
 def build_list_of_words(file):
@@ -22,7 +21,7 @@ def build_set_of_words(file):
     return {word for word in file}
 
 
-def average_runtime(data_structure, word):
+def average_runtime(data_structure, word: str):
     """
     Gets a data-structure and a word, and search the word a 1000 times while calculating the time that it tooks
     and returns the time
@@ -44,8 +43,38 @@ my_set = build_set_of_words(long_words_file)
 print(f"time of search in list: {average_runtime(my_list, 'zwitterion')}")
 print(f"time of search in set: {average_runtime(my_set, 'zwitterion')}")
 
+print("----------------------------------------------------------\n")
 long_words_file.close()
 
-#############################################
-#############################################
 
+#############################################
+#############################################
+# -- שטחולנדיה
+
+
+def is_in_set(country: str, line_keyboard: set):
+    for ch in country:
+        if ch not in line_keyboard:
+            return False
+    return True
+
+
+def find_special_state(file):
+    """
+    Find the state that all of its characters are in one line in a regular keyboard
+    :param file: A text file including names of countries in USA
+    :return: The name of the country that we can write in one line in the keyboard
+    """
+    my_set1 = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '\n'}
+    my_set2 = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\n'}
+    my_set3 = {'z', 'x', 'c', 'v', 'b', 'n', 'm', '\n'}
+
+    for my_set_line in [my_set1, my_set2, my_set3]:
+        for country in file:
+            if is_in_set(country, my_set_line):
+                return country
+        file.seek(0)
+
+
+text_file = open("resources/states.txt")
+print(find_special_state(text_file))
