c40c92bef0ab08c78d5dd340562bf85b1d675419
Eliachar Feig <efeig15@gmail.com>
2022-03-12 19:08:51 +0200

    Update week6_2.py

--- Ex1/week6_2.py
+++ Ex1/week6_2.py
@@ -20,6 +20,13 @@ def divide(num1, num2):
 
 
 def calc(num1, num2, math_sign):
+    """
+    Gets 2 numbers and a math sign (+, -, * or /) and return the result by using a dictionary of functions
+    :param num1: First number
+    :param num2: Second number
+    :param math_sign: A math sign
+    :return:
+    """
     dictionary = {
         '+': add(num1, num2),
         '-': subtract(num1, num2),
@@ -38,36 +45,48 @@ print("-------------------------------------------")
 # From here to here
 
 
-def square(number):
-    return number ** 2
-
-
 def apply(func, iterable):
+    """
+    A generator that gets a function and an iterable and on each item of the iterable return the result
+    of the item after activate the function on it
+    :param func: A function
+    :param iterable: An iterable
+    :return: Activation of the function on each item of the iterable
+    """
     for item in iterable:
         yield func(item)
 
 
-square_check = apply(square, [5, -1, 6, -8, 0])
+square_check = apply(lambda number: number ** 2, [5, -1, 6, -8, 0])
 print(tuple(square_check) == (25, 1, 36, 64, 0))
 print("------------------------------------------------")
 ###########################################################
 # map
 
 
-def generate_first_names(lst):
-    return map(lambda name:  name.split(' ', 1)[0], lst)
+def generate_first_names(lst_full_names):
+    """
+    Generator function that get a list of full names and returns an iterator that point only to the first names
+    :param lst_full_names: A list of full names (using map)
+    :return: An iterator of the first names only
+    """
+    return map(lambda name:  name.split(' ', 1)[0], lst_full_names)
 
 
 for first_name in generate_first_names(['Bob Smith', 'Alice Johnson', 'Ali Baba']):
     print(first_name)
-
 print("----------------------------------------------")
 ##########################################################
 # filter
 
 
-def generate_only_polyndromes(lst):
-    return filter(lambda word: word == word[::-1], lst)
+def generate_only_polyndromes(lst_of_words):
+    """
+    Generator that get list of words and returns a generator of the words that are polyndromes only
+    :param lst_of_words: A list of words (using filter)
+    :return: An iterator on the words that are polyndromes
+    """
+    return filter(lambda word: word == word[::-1], lst_of_words)
 
 
 for polyndrome in generate_only_polyndromes(['abba', 'bob', 'week6_2', '1234', 'python', 'ArqqrA', 'WoW']):
@@ -84,7 +103,9 @@ closet = [
     {'name': 'Jadis', 'year_of_birth': 0, 'gender': 'Female'},
 ]
 
-print(sorted(closet, key=lambda d: d['name'][-1]))
+print(f"Sorted by the last character of the name: ")
+print('\n'.join(map(str,
+                    sorted(closet, key=lambda d: d['name'][-1]))))
 
 print("---------------------------------------------------")
 ###########################################################
@@ -92,6 +113,12 @@ print("---------------------------------------------------")
 
 
 def my_filter(func, iterable):
+    """
+    A generator function that implement the known function "filter of python
+    :param func: A function
+    :param iterable: An iterable
+    :return: An iterator of the items in the iterable that return True from the function sent
+    """
     for item in iterable:
         if func(item):
             yield item
@@ -106,7 +133,12 @@ print("--------------------------------------------")
 
 
 def get_positive_numbers(inp):
-    return list(filter(lambda x: x != ',' and (int(x) % 2) == 0, inp))
+    """
+    A function that gets an input of numbers seperated by ',' and return a list of the positive numbers
+    :param inp: The input from the user
+    :return: list of all positives
+    """
+    return list(filter(lambda x: int(x) > 0, list(map(int, inp.split(",")))))
 
 
 print(get_positive_numbers(input("Enter a serie of numbers separated by ',': \n")))
@@ -117,6 +149,12 @@ print("--------------------------------------------------------\n")
 
 
 def timer(f, *args):
+    """
+    A function that check out the time that to the function passed to run on the arguments passed
+    :param f: A function
+    :param args: A list of arguments
+    :return: The time that the function passed run on the arguments passed
+    """
     start_time = time.time()
     f(args)
     return time.time() - start_time
@@ -124,7 +162,6 @@ def timer(f, *args):
 
 print(timer(print, "Hello"))
 print(timer(zip, [1, 2, 3], [4, 5, 6]))
-#print(timer("Hi {name}".format, name="Bug"))
 
 
 
