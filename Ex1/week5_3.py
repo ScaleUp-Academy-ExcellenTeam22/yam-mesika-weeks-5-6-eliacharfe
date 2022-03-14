675d058b0b762be72cfbf3f858782efd34f4ec6f
Eliachar Feig <efeig15@gmail.com>
2022-03-15 01:44:44 +0200

    Update week5_3.py

--- Ex1/week5_3.py
+++ Ex1/week5_3.py
@@ -9,12 +9,11 @@ def find_all_hidden_messages_in_a_binary_file_via_pattern(path, pattern):
     as regular strings.
     :return: A list of the hidden messages
     """
+    reg = re.compile(pattern)
     with open(path, "rb") as encrypted_binary_file:
         list_hidden_messages = []
-        for line in encrypted_binary_file.readlines():
-            hidden_messages = re.compile(pattern).findall(line)
-            if hidden_messages:
-                list_hidden_messages += [hidden_msg.decode('ascii') for hidden_msg in hidden_messages]
+        list_hidden_messages += [hidden_msg.decode('ascii') for line in encrypted_binary_file.readlines()
+                                 if reg.findall(line) for hidden_msg in reg.findall(line)]
         return list_hidden_messages
 
 
@@ -22,3 +21,14 @@ if __name__ == '__main__':
     print('\n'.join(
         find_all_hidden_messages_in_a_binary_file_via_pattern("resources/logo.jpg", b'[a-z]{5,}!')))
 
+
+
+
+
+
+    # with open(path, "rb") as encrypted_bin_file:
+    #     list_hidden_messages = []
+    #     for line in encrypted_binary_file.readlines():
+    #         if re.compile(pattern).findall(line):
+    #             lst_hidden_messages += [hidden_msg.decode('ascii') for hidden_msg in re.compile(pattern).findall(line)]
+    #     return lst_hidden_messages
