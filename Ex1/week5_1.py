34fc0f98a0e62d4c595720d68b20683d71b5f7d8
Eliachar Feig <efeig15@gmail.com>
2022-03-15 09:34:16 +0200

    Fix requested changes

--- Ex1/week5_1.py
+++ Ex1/week5_1.py
@@ -5,53 +5,47 @@ import random
 # That's the way
 
 
-def find_files_in_directory_startwith_deep(path):
+def find_files_with_deep_prefix(directory_path):
     """
     This function gets the path to a directory and return a list of files that start with the string "deep"
-    :param path: - The path to the directory
     :return: List of files that start with "deep"
     """
-    return [file for file in os.listdir(path) if file.startswith("deep")]
+    return [file for file in os.listdir(directory_path) if file.startswith("deep")]
 
 
 # I don't have vinaigrette
 
 
-def get_random_date_between(date1, date2):
+def get_random_date_between(first_date, second_date):
     """
-    The function gets 2 dates and return a random date between those dates
-    :param date1: a date from the input in format "YYYY-MM-DD"
-    :param date2: a date from the input in format "YYYY-MM-DD"
-    :return: random date between the dates
+    The function gets 2 dates and return a random date between those dates.
     """
-    random_number_of_days = random.randrange(abs(date1 - date2).days)
-    random_date = date1 + datetime.timedelta(random_number_of_days)
-    return str(random_date)
+    random_number_of_days = random.randrange(abs(first_date - second_date).days)
+    random_date_between_dates = first_date + datetime.timedelta(random_number_of_days)
+    return random_date_between_dates
 
 
-def random_date_between_dates_check_if_monday(date1, date2):
+def random_date_between_dates_check_if_monday(first_input_date, second_input_date):
     """
-     The function gets 2 dates and after generate a random number between them print "I dont have vinaigrette
-     if the the random date fall on monday
-    :param date1: a date from the input in format "YYYY-MM-DD"
-    :param date2: a date from the input in format "YYYY-MM-DD"
-    :return: none
+     The function gets 2 dates and after generate a random number between then print "I dont have
+     vinaigrette" if the the random date fall on monday.
     """
-    random_date = get_random_date_between(date1, date2)
-    print("random date = " + random_date)
-    if datetime.datetime.strptime(random_date, "%Y-%m-%d").date().isoweekday() == 2:
+    try:
+        first_date = datetime.datetime.strptime(first_input_date, "%Y-%m-%d").date()
+        second_date = datetime.datetime.strptime(second_input_date, "%Y-%m-%d").date()
+    except ValueError:
+        print("Wrong format: the correct format is: YYYY-MM-DD")
+        return
+    random_date = get_random_date_between(first_date, second_date)
+    print("Random date between is: " + str(random_date))
+    if random_date.isoweekday() == 2:
         print("I dont have vinaigrette")
 
 
-if __name__ == '__main__':
-    print(f"list of files that start with deep: {find_files_in_directory_startwith_deep('images')}")
+if __name__ == "__main__":
+    print(f"List of files that start with deep: {find_files_with_deep_prefix('images')}")
     print("--------------------------------------\n")
 
-    date_1 = input("Enter a date\n")
-    date_2 = input("Enter a second date\n")
-    try:
-        random_date_between_dates_check_if_monday(datetime.datetime.strptime(date_1, "%Y-%m-%d").date(),
-                                                  datetime.datetime.strptime(date_2, "%Y-%m-%d").date())
-    except ValueError:
-        print("Wrong format: the correct format is: YYYY-MM-DD")
+    random_date_between_dates_check_if_monday(input("Enter a date\n"), input("Enter a second date\n"))
+
 
