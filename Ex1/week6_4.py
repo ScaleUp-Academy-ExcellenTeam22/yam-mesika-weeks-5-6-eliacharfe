573297e84159422af2061fba40861cbe0c25732a
Eliachar Feig <efeig15@gmail.com>
2022-03-27 02:30:11 +0300

    Corrections

--- Ex1/week6_4.py
+++ Ex1/week6_4.py
@@ -1,20 +1,17 @@
 from PIL import Image
 
 
-def decoding_message_in_image(path):
+def decoding_message_in_image(image_path):
     """
-    In each column of the image there is a pixel painted black.
-    The pixel is colored by the line number that corresponds to the numeric value of the character.
-    If you convert the position where the black pixels are in order, from left to right, you get the
-    hidden message.
-    :param path: The path to an image
-    :return: The hidden message
+    The function decodes a hidden message hidden in in each column of the image that the line number
+    is corresponds to the numeric value of the character pixels if we take from left to right order.
+    :param image_path: The path to an image.
+    :return: The hidden message.
     """
-    img = Image.open(path)
+    img = Image.open(image_path)
     pixels = img.load()
-    width = img.size[0]
-    height = img.size[1]
-    return ''.join([chr(row) for col in range(width) for row in range(height) if pixels[col, row] == 1])
+    width, height = img.size[:2]
+    return "".join([chr(row) for col in range(width) for row in range(height) if pixels[col, row] == 1])
 
 
 if __name__ == '__main__':
