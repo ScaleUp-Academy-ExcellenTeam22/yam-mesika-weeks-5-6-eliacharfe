b4d9d60e28f5f2d2d5018d021e79083ccf0b4e1c
Eliachar Feig <efeig15@gmail.com>
2022-03-27 01:52:49 +0300

    Corrections

--- Ex1/week6_1.py
+++ Ex1/week6_1.py
@@ -1,24 +1,23 @@
 import time
+from typing import IO
 
-# -- Fast and Reliable
 
-
-def build_list_of_words(file):
+def build_list_of_words(text_file: IO[str]):
     """
     Build a list of words from a file given
-    :param file: The file containing words
+    :param text_file: The file containing words
     :return: A list of the words
     """
-    return [word for word in file]
+    return [word for word in text_file]
 
 
-def build_set_of_words(file):
+def build_set_of_words(text_file: IO[str]):
     """
     Build a set of words from a file given
-    :param file: The file containing words
+    :param text_file: The file containing words
     :return: A set of the words
     """
-    return {word for word in file}
+    return {word for word in text_file}
 
 
 def average_runtime(data_structure, word: str):
@@ -32,24 +31,18 @@ def average_runtime(data_structure, word: str):
     start_time = time.time()
     for i in range(0, 1000):
         if word in data_structure:
-            pass
+            continue
     return time.time() - start_time
 
 
-# -- שטחולנדיה
-
-
 def is_in_set(country: str, line_keyboard: set):
-    for ch in country:
-        if ch not in line_keyboard:
-            return False
-    return True
+    return set(country).intersection(line_keyboard)
 
 
-def find_special_state(file):
+def find_special_state(text_file: IO[str]):
     """
     Find the state that all of its characters are in one line in a regular keyboard
-    :param file: A text file including names of countries in USA
+    :param text_file: A text file including names of countries in USA
     :return: The name of the country that we can write in one line in the keyboard
     """
     my_set1 = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '\n'}
@@ -57,10 +50,10 @@ def find_special_state(file):
     my_set3 = {'z', 'x', 'c', 'v', 'b', 'n', 'm', '\n'}
 
     for my_set_line in [my_set1, my_set2, my_set3]:
-        for country in file:
+        for country in text_file:
             if is_in_set(country, my_set_line):
                 return country
-        file.seek(0)
+        text_file.seek(0)
 
 
 if __name__ == '__main__':
@@ -70,7 +63,6 @@ if __name__ == '__main__':
     print(f"time of search in list: {average_runtime(my_list, 'zwitterion')}")
     print(f"time of search in set: {average_runtime(my_set, 'zwitterion')}")
     long_words_file.close()
-    print("----------------------------------------------------------")
 
-    text_file = open("resources/states.txt")
-    print(find_special_state(text_file))
+    my_text_file = open("resources/states.txt")
+    print(find_special_state(my_text_file))
