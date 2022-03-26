afd51ecef7c13f586be9f58daa45ab8cb990e59f
Eliachar Feig <efeig15@gmail.com>
2022-03-27 01:19:05 +0300

    Corrections

--- Ex1/week5_3.py
+++ Ex1/week5_3.py
@@ -2,6 +2,10 @@
 import re
 
 
+def open_binary_file(path):
+    return open(path, "rb")
+
+
 def find_all_hidden_messages_in_a_binary_file_via_pattern(path, pattern):
     """
     The function gets a path to a binary file, and a binary pattern and search all "strings" that
@@ -10,24 +14,14 @@ def find_all_hidden_messages_in_a_binary_file_via_pattern(path, pattern):
     :return: A list of the hidden messages
     """
     reg = re.compile(pattern)
-    with open(path, "rb") as encrypted_binary_file:
-        return [''.join(msg) for msg in [hidden_msg.decode('ascii')
-                for line in encrypted_binary_file.readlines()
-                if reg.findall(line) for hidden_msg in reg.findall(line)]]
+    encrypted_binary_file = open_binary_file(path)
+
+    lst_hidden_messages = [hidden_msg.decode('ascii') for line in encrypted_binary_file.readlines()
+                           if reg.findall(line) for hidden_msg in reg.findall(line)]
+    encrypted_binary_file.close()
+    return ["".join(msg) for msg in lst_hidden_messages]
 
 
 if __name__ == '__main__':
     print('\n'.join(
         find_all_hidden_messages_in_a_binary_file_via_pattern("resources/logo.jpg", b'[a-z]{5,}!')))
-
-
-
-
-
-
-    # with open(path, "rb") as encrypted_bin_file:
-    #     list_hidden_messages = []
-    #     for line in encrypted_binary_file.readlines():
-    #         if re.compile(pattern).findall(line):
-    #             lst_hidden_messages += [hidden_msg.decode('ascii') for hidden_msg in re.compile(pattern).findall(line)]
-    #     return lst_hidden_messages
