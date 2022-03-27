71efac105925939d274b8d9994c9a6feaf62b853
Eliachar Feig <efeig15@gmail.com>
2022-03-27 22:26:37 +0300

    Update week5_1.py

--- Ex1/week5_1.py
+++ Ex1/week5_1.py
@@ -2,10 +2,8 @@ import datetime
 import os
 import random
 
-# That's the way
 
-
-def find_files_with_deep_prefix(directory_path):
+def find_files_with_deep_prefix(directory_path: str):
     """
     This function gets the path to a directory and return a list of files that start with the string "deep"
     :return: List of files that start with "deep"
@@ -13,12 +11,10 @@ def find_files_with_deep_prefix(directory_path):
     return [file for file in os.listdir(directory_path) if file.startswith("deep")]
 
 
-# I don't have vinaigrette
-
-
-def get_random_date_between(first_date, second_date):
+def get_random_date_between(first_date: datetime.date, second_date: datetime.date):
     """
     The function gets 2 dates and return a random date between those dates.
+    Taken from the internet.
     """
     random_number_of_days = random.randrange(abs(first_date - second_date).days)
     random_date_between_dates = first_date + datetime.timedelta(random_number_of_days)
@@ -44,7 +40,6 @@ def random_date_between_dates_check_if_monday(first_input_date, second_input_dat
 
 if __name__ == "__main__":
     print(f"List of files that start with deep: {find_files_with_deep_prefix('images')}")
-    print("--------------------------------------\n")
 
     random_date_between_dates_check_if_monday(input("Enter a date\n"), input("Enter a second date\n"))
 
